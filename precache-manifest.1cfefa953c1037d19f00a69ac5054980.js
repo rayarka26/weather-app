e7daf4a9a275c6151706f05785c1a67514cafb62
rayarka26 <74251803+rayarka26@users.noreply.github.com>
2023-04-11 22:06:35 +0530

    Updates

---
+++ precache-manifest.1cfefa953c1037d19f00a69ac5054980.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a58fa419b19f6b4e2fed909a22022e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "145169f46bce0067b0ce",
+    "url": "/weather-app/static/css/main.7e6b9481.chunk.css"
+  },
+  {
+    "revision": "001b67d05a335e326bff",
+    "url": "/weather-app/static/js/2.6a7fe4e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.6a7fe4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "145169f46bce0067b0ce",
+    "url": "/weather-app/static/js/main.7f03a703.chunk.js"
+  },
+  {
+    "revision": "012abc6174db71a893dc",
+    "url": "/weather-app/static/js/runtime-main.938664af.js"
+  },
+  {
+    "revision": "0a218296b39a3569fe9c933e3ababb49",
+    "url": "/weather-app/static/media/WeatherIcons.0a218296.gif"
+  },
+  {
+    "revision": "89c90cd092419cad3b5f7ff356b9c35c",
+    "url": "/weather-app/static/media/background.89c90cd0.jpg"
+  },
+  {
+    "revision": "ab4f2c217b192bbbfa076a063dc26734",
+    "url": "/weather-app/static/media/city.ab4f2c21.jpg"
+  }
+]);
